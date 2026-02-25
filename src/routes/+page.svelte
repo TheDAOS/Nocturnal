d14f06d0a096e68870d47927c80a7e4e94b53855
Sanju Santhosh <68913047+TheDAOS@users.noreply.github.com>
2026-02-25 23:16:08 +0000

    style: update font styles and layout for consistency across components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,8 @@
 	import HelloWorld from '$components/HelloWorld.svelte';
 </script>
 
-<main class="flex min-h-screen min-w-screen flex-col items-center bg-background">
-	<h1 class="text-4xl font-bold text-soft-black">Welcome to Nocturnal!</h1>
+<main class="flex min-h-screen min-w-screen flex-col items-center bg-background font-body">
+	<h1 class="text-4xl font-title text-soft-black">Welcome to Nocturnal!</h1>
 	<HelloWorld name="Nocturnal" />
 </main>
 
